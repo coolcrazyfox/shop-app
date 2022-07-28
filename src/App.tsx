4585017e25ef03b646f9d34cf508e5709acac17b
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-29 00:10:27 +0300

    create component CreateProduct

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {IProduct} from "./models";
 function App() {
     // const [count, setCount] = useState(0)
     const {loading, error, products, addProduct} = useProducts()
-    const [modal, setModal] = useState(true)
+    const [modal, setModal] = useState(false)
     const createHandler = (product:IProduct)=>{
         setModal(false)
         addProduct(product)
@@ -25,9 +25,13 @@ function App() {
             {/*<Product product={products[0]}/>*/}
             {/*<h1>`hello {count}`</h1>*/}
             {/*<button onClick={() => setCount(count + 1)}>click</button>*/}
-            {modal && <Modal title={'Create new product'}>
+            {modal && <Modal title={'Create new product'} onClose={() => setModal(false)}>
                 <CreateProduct onCreate={createHandler}/>
             </Modal>}
+            <button
+                onClick={()=> setModal(true)}
+                className={"fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"}
+            > + </button>
 
         </div>
     );
