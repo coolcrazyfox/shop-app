174dd63595b8ddbc0c316bb0805f3f0061909099
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-29 00:27:35 +0300

    create ModalContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import Loader from './components/Loader';
 import Product from "./components/Product";
 import {useProducts} from "./hooks/products";
@@ -6,14 +6,17 @@ import ErrorMessage from "./components/ErrorMessage";
 import Modal from "./components/Modal";
 import CreateProduct from "./components/CreateProduct";
 import {IProduct} from "./models";
+import {ModalContext} from "./context/ModalContext";
 
 
 function App() {
     // const [count, setCount] = useState(0)
     const {loading, error, products, addProduct} = useProducts()
-    const [modal, setModal] = useState(false)
+    const {modal, open, close}=useContext(ModalContext)
+    // const [modal, setModal] = useState(false)
     const createHandler = (product:IProduct)=>{
-        setModal(false)
+        close()
+        // setModal(false)
         addProduct(product)
     }
 
@@ -25,11 +28,11 @@ function App() {
             {/*<Product product={products[0]}/>*/}
             {/*<h1>`hello {count}`</h1>*/}
             {/*<button onClick={() => setCount(count + 1)}>click</button>*/}
-            {modal && <Modal title={'Create new product'} onClose={() => setModal(false)}>
+            {modal && <Modal title={'Create new product'} onClose={close}>
                 <CreateProduct onCreate={createHandler}/>
             </Modal>}
             <button
-                onClick={()=> setModal(true)}
+                onClick={open}
                 className={"fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2"}
             > + </button>
 
