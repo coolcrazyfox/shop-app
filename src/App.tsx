9ce19b96519c60de7173e35f87a139d1b9a680c0
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-28 22:41:35 +0300

    changing components App and Product

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Product from "./components/Product";
+import {useProducts} from "./hooks/products";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [count, setCount] = useState(0)
+    const {loading, error, products}=useProducts()
+
+    return (
+        <div className="container mx-auto max-w-2xl pt-5">
+            {loading && <h2 className="text-center">Loading...</h2>}
+            {error && <p className="text-center text-red-600">{error}</p>}
+            {products.map(pr => <Product product={pr} key={pr.id}/>)}
+            {/*<Product product={products[0]}/>*/}
+            {/*<h1>`hello {count}`</h1>*/}
+            {/*<button onClick={() => setCount(count + 1)}>click</button>*/}
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Product.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {IProduct} from "../models";
+
+interface ProductProps {
+    product: IProduct
+
+}
+
+const Product = ({product}: ProductProps) => {
+    // const Product = (props:ProductProps) => {
+    // props.product.rating.count
+    const [details, setDetails] = useState(false)
+    const btnBgClassName = details ? 'bg-red-400' : 'bg-blue-400'
+    const btnClasses = ['py-2 px-4 border', btnBgClassName]
+
+    return (
+        <div className='border py-2 px-4 rounded flex flex-col items-center mb-2'>
+            {/*{props.product.title}*/}
+            <img src={product.image} alt={product.title} className="w-1/6 bg"/>
+            <p>{product.title}</p>
+            <span className="font-bold">{product.price}$</span>
+            <button className={btnClasses.join('')}
+                    onClick={() => setDetails(prev => !prev)}
+            >
+                {details ? 'Hide Details' : 'Show Details'}
+            </button>
+            {details && <div>
+                <p>{product.description}</p>
+                <p>Rating: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+            </div>}
+
+        </div>
+    );
+};
+
+export default Product;
