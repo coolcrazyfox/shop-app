6904f9c8b4264dd6715a6dfd6c43f0b027f77898
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-30 23:58:44 +0300

    create HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {ModalState} from "./context/ModalContext";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter, HashRouterProps} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <ModalState>
             <App />
         </ModalState>
-    </BrowserRouter>
+    </HashRouter>
 
 
 );
