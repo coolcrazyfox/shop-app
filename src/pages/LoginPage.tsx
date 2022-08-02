f9d46847a1d9823e539a1517ff6232436355bfb8
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-02 23:35:51 +0300

    changing LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import s from './Login.module.css'
 
 const LoginPage = () => {
     return (
         <section>
-            <div className={'color'} ></div>
-            <div className={'color'}></div>
-            <div className={'color'}></div>
-            <div className={'box'}>
-                <div className="container">
-                    <div className="form">
+            <div className={s.color} ></div>
+            <div className={s.color}></div>
+            <div className={s.color}></div>
+            <div className={s.box}>
+                <div className={s.container}>
+                    <div className={s.form}>
                         <h2>Login Form</h2>
                         <form >
-                            <div className="inputBox">
+                            <div className={s.inputBox}>
                                 <input type="text" placeholder={"Username"}/>
                             </div>
-                            <div className="inputBox">
+                            <div className={s.inputBox}>
                                 <input type="password" placeholder={"Password"}/>
                             </div>
-                            <div className="inputBox">
+                            <div className={s.inputBox}>
                                 <input type="submit" value={"Login"}/>
                             </div>
-                            <p className="forget">Forgot Password?<a href={'#'}>Click here</a></p>
-                            <p className="forget">Don't have an account?<a href={'#'}>Sing up</a></p>
+                            <p className={s.forget}>Forgot Password?<a href={'#'}>Click here</a></p>
+                            <p className={s.forget}>Don't have an account?<a href={'#'}>Sing up</a></p>
                         </form>
                     </div>
                 </div>
