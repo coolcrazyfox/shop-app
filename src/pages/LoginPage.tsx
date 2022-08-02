3ab6c103e79caf4dc4b44fdb40c441ca5cc658bf
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-02 22:57:44 +0300

    changing LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,6 +6,26 @@ const LoginPage = () => {
             <div className={'color'} ></div>
             <div className={'color'}></div>
             <div className={'color'}></div>
+            <div className={'box'}>
+                <div className="container">
+                    <div className="form">
+                        <h2>Login Form</h2>
+                        <form >
+                            <div className="inputBox">
+                                <input type="text" placeholder={"Username"}/>
+                            </div>
+                            <div className="inputBox">
+                                <input type="password" placeholder={"Password"}/>
+                            </div>
+                            <div className="inputBox">
+                                <input type="submit" value={"Login"}/>
+                            </div>
+                            <p className="forget">Forgot Password?<a href={'#'}>Click here</a></p>
+                            <p className="forget">Don't have an account?<a href={'#'}>Sing up</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
         </section>
     );
