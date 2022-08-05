389fbecde7216ef7ce4537972bbbf004fe8f23d7
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-05 23:28:14 +0300

    changing RangeSlider.tsx

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from "../styles/Login.module.css";
 
 const ContactsPage = () => {
     return (
-        <div>
-            My contacts
+        <div className={s.form}>
+            <h2 data-text="&nbsp;Contact">&nbsp;Contact</h2>
         </div>
     );
 };
