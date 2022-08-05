736d0f7f2be3cf06f33694148dc19be4a6257142
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-05 23:26:50 +0300

    changing RangeSlider.tsx

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from "../styles/Login.module.css";
 
 const ErrorPage = () => {
     return (
-        <div>
-           Error 404
+        <div className={s.form}>
+            <h2 data-text="&nbsp;Error">&nbsp;Error</h2>
         </div>
     );
 };
