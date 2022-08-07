0f240bbcf1395926c965bc0466f72dfc02d1a442
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-07 23:02:03 +0300

    changing ContactsPage.tsx

--- src/pages/ContactsPage.tsx
+++ src/pages/ContactsPage.tsx
@@ -3,9 +3,41 @@ import s from "../styles/Login.module.css";
 
 const ContactsPage = () => {
     return (
-        <div className={s.form}>
-            <h2 data-text="&nbsp;Contact">&nbsp;Contact</h2>
-        </div>
+        <section>
+            <div className={s.color} ></div>
+            <div className={s.color}></div>
+            <div className={s.color}></div>
+            <div className={s.box}>
+                <div className={s.square} ></div>
+                <div className={s.square} ></div>
+                <div className={s.square} ></div>
+                <div className={s.square} ></div>
+                <div className={s.square} ></div>
+                {/*<div className="square" style={{"--i:0;"}}></div>*/}
+                {/*<div className="square" style="--i:1;"></div>*/}
+                {/*<div className="square" style="--i:2;"></div>*/}
+                {/*<div className="square" style="--i:3;"></div>*/}
+                <div className={s.container}>
+                    <div className={s.form}>
+                        <h2 data-text="&nbsp;Contacts">&nbsp;Contact</h2>
+                        <form >
+                            <div className={s.inputBox}>
+                                <input type="text" placeholder={"Username"}/>
+                            </div>
+                            <div className={s.inputBox}>
+                                <input type="password" placeholder={"Password"}/>
+                            </div>
+                            <div className={s.inputBox}>
+                                <input type="submit" value={"Login"}/>
+                            </div>
+                            <p className={s.forget}>Forgot Password ? <a href={'#'}>Click here</a></p>
+                            <p className={s.forget}>Don't have an account ? <a href={'#'}>Sing up</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     );
 };
 
