4254d9bf17dd89b27caccaed5eadc361e14b846d
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-15 19:00:58 +0300

    create component Broken.tsx

--- src/components/Broken.tsx
+++ src/components/Broken.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Header = () => {
+const Broken = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
 
-export default Header;
+export default Broken;
